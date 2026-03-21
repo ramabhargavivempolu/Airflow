7f6d6d0e2595500e31ef7b9bd0e8fc0aa680b923
ramabhargavivempolu <ramabhargavivempolu5@gmail.com>
2026-03-21 17:07:57 -0400

    Initial commit

---
+++ dags/1_first_dag.py
@@ -0,0 +1,31 @@
+from airflow.sdk import dag, task
+from more_itertools import first
+
+# Define the DAG using the decorator - function within function
+@dag(
+    dag_id="first_dag",
+)
+def first_dag():
+
+    @task.python
+    def first_task():
+        print("This is the first task")
+    
+    @task.python
+    def second_task():
+        print("This is the second task")
+    
+    @task.python
+    def third_task():
+        print("This is the third task")
+
+    # Define the task dependencies
+    first = first_task()
+    second = second_task()
+    third = third_task()
+
+    first >> second >> third
+
+# Instantiate the DAG
+first_dag()
+
